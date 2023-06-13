ffbf5e3124f20dcf1990dd90608f1be76d6d55b0
Estevão Moretti Gouveia <135063244+egouve@users.noreply.github.com>
2023-06-13 20:40:30 -0300

    Inserido exercício 1

---
+++ Day-3/Day-3.1.py
@@ -0,0 +1,11 @@
+#Exercício: criar um programa que entenda se o número inputado é par ou impar
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
