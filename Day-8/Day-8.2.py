0740837b61a4aba67ba7b3ecac1c3272b37fa421
Estevão Moretti Gouveia <135063244+egouve@users.noreply.github.com>
2023-06-25 20:23:55 -0300

    Dia 8 exercício 2

--- Day-8/Day-8.2.py
+++ Day-8/Day-8.2.py
@@ -1,14 +1,12 @@
 # Checar se é número primo
 
 def checar_numero_primo(numero):
-    for n in range(2, 8):
-        if numero % numero == 0 and numero % n != 0:
-            print('É primo')
-            return
-        else:
-            print('Não é primo')
+    for n in range(2, numero):
+        if numero % n == 0:
+            print("Não é primo")
             return
 
+    print("É primo")
 
 numero = int(input('Escolha um número: '))
 
