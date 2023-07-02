6026151e69b16df47950119e50692feb3872eb80
Estevão Moretti Gouveia <135063244+egouve@users.noreply.github.com>
2023-07-01 21:09:45 -0300

    Dia 8 exercício 3

---
+++ Day-8/Day-8.3.py
@@ -0,0 +1,20 @@
+# Criar uma função que criptografe uma mensagem
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def criptografar(text, shift):
+    palavra_nova = []
+    for letter in text:
+        index_letra = alphabet.index(letter)
+        nova_letra = alphabet[index_letra + shift]
+        palavra_nova.append(nova_letra)
+
+    palavra_nova = ''.join(palavra_nova)
+    print(palavra_nova)
+
+criptografar(text,shift)
